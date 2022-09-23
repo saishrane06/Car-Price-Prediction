2dc86df4d59f685dd7063e5642ccfb247bfdb580
Saish Rane <saishrane06@gmail.com>
2022-09-23 14:55:49 +0530

    checked regmodel.pkl with postman for the output and also made some changes in app.py

--- app.py
+++ app.py
@@ -21,10 +21,10 @@ def predict_api():
     data=request.json['data']
     print(data)
     print(np.array(data))
-    new_data=scalar.transform(np.array(data))
+    new_data=scalar.transform(np.array(list(data.values())).reshape(1, -1))
     output=regmodel.predict(new_data)
     print(output[0])
     return jsonify(output[0])
 
-if __name__=="__main":
+if __name__=="__main__":
     app.run(debug=True)
